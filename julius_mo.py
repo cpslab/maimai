effb878832806c5e553459751de114f824774f5f
elzup <guild0105@gmail.com>
2016-05-26 19:27:04 +0900

    Mod: start base controll

--- julius_mo.py
+++ julius_mo.py
@@ -30,6 +30,7 @@ def main():
 
     text_queue = ""
     separator = ".\n"
+    is_state_recieve = False
 
     while True:
         recv_data = sock.recv(bufsize)
@@ -39,21 +40,42 @@ def main():
         if separator in text_queue:
             parts = text_queue.split(separator)
             xmls, text_queue = parts[:-1], parts[-1]
-            print(xmls)
+            # print(xmls)
+            print(">")
+            # 前回受け取ったコマンドが 'start' であるか
             for xml_text in xmls:
                 try:
                     root = ET.fromstring(xml_text)
                     for word in root.iter('WHYPO'):
+                        print(word)
+                        print(word.attrib)
                         command = word.get('WORD')
-                        start(command)
-                    print(root)
+                        print(is_state_recieve)
+                        if command == 'start':
+                            is_state_recieve = True
+                            say('はい')
+                        elif is_state_recieve:
+                            is_state_recieve = False
+                            start(command)
                 except ET.ParseError:
                     print('parce error--')
                     print(xml_text)
                     print('--')
     
+# Commands
 def start(q):
-    say(q)
+    if q == 'how_weather':
+        say('天気は晴れです')
+    elif q in ['what_day', 'what_date']:
+        say('今日は月曜日です')
+    elif q == 'is_sasago':
+        say('今日は笹子の日です')
+    elif q == 'run_make_coffee':
+        say('コーヒーはまだコントロールできません')
+    elif q == 'run_light_on':
+        say('電気はまだコントロールできません')
+    elif q == 'run_light_off':
+        say('電気はまだコントロールできません')
 
 def fix_format_xml(text):
     return "".join(text.split("\n")[:-2])
